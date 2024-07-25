6bcf1b78940013ec100413b8900e74ef7d2b615e
tudor <7089284+tudddorrr@users.noreply.github.com>
2024-07-25 20:36:07 +0100

    fix up error handling in verify example

--- addons/talo/samples/authentication/scripts/verify.gd
+++ addons/talo/samples/authentication/scripts/verify.gd
@@ -13,7 +13,9 @@ func _on_submit_pressed() -> void:
   var res = await Talo.player_auth.verify(code.text)
   if res != OK:
     match Talo.player_auth.last_error.get_code():
-      TaloAuthError.ErrorCode.INVALID_CREDENTIALS:
+      TaloAuthError.ErrorCode.VERIFICATION_CODE_INVALID:
         validation_label.text = "Verification code is incorrect"
+      TaloAuthError.ErrorCode.VERIFICATION_ALIAS_NOT_FOUND:
+        validation_label.text = "Verification session is invalid"
       _:
         validation_label.text = Talo.player_auth.last_error.get_string()
