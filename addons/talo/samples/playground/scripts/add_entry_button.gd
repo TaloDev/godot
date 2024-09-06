faa0ca4a7a88475947346a4f2bda1fb8e8a99bd4
tudor <7089284+tudddorrr@users.noreply.github.com>
2024-09-06 23:57:03 +0100

    handle bad leaderboard responses in playground

--- addons/talo/samples/playground/scripts/add_entry_button.gd
+++ addons/talo/samples/playground/scripts/add_entry_button.gd
@@ -7,4 +7,5 @@ func _on_pressed() -> void:
 	var score = RandomNumberGenerator.new().randi_range(1, 50)
 	var res = await Talo.leaderboards.add_entry(leaderboard_name, score)
 
-	response_label.text = "Added score: %s, new high score: %s" % [score, res[1]]
+	if res.size() > 0:
+		response_label.text = "Added score: %s, new high score: %s" % [score, res[1]]
