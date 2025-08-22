1d7b7184a2a831318142d5166303ac99f5cf7e03
tudor <7089284+tudddorrr@users.noreply.github.com>
2025-08-22 19:35:26 +0100

    paginate player group members

--- addons/talo/apis/player_groups_api.gd
+++ addons/talo/apis/player_groups_api.gd
@@ -5,12 +5,26 @@ class_name PlayerGroupsAPI extends TaloAPI
 ##
 ## @tutorial: https://docs.trytalo.com/docs/godot/groups
 
-## Get the members of a group. If the group is public (as set in the dashboard), the members will be returned. If the group is private, only a membership count will be returned.
-func get_group(group_id: String) -> TaloPlayerGroup:
-	var res := await client.make_request(HTTPClient.METHOD_GET, "/%s" % group_id)
+## Get the members of a group. If the group is public (as set in the dashboard), the members will be returned. If the group is private, only a membership count will be returned. Members are paginated and a page number can be specified.
+func get_group(group_id: String, page: int = 0) -> GroupPage:
+	var res := await client.make_request(HTTPClient.METHOD_GET, "/%s?page=%s" % [group_id, page])
 
 	match res.status:
 		200:
-			return TaloPlayerGroup.new(res.body.group)
+			var group = TaloPlayerGroup.new(res.body.group)
+			var pagination = res.body.membersPagination
+			return GroupPage.new(group, pagination.count, pagination.itemsPerPage, pagination.isLastPage)
 		_:
 			return null
+
+class GroupPage:
+	var group: TaloPlayerGroup
+	var count: int
+	var items_per_page: int
+	var is_last_page: bool
+
+	func _init(group: TaloPlayerGroup, count: int, items_per_page: int, is_last_page: bool) -> void:
+		self.group = group
+		self.count = count
+		self.items_per_page = items_per_page
+		self.is_last_page = is_last_page
