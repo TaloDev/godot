073bd1575a33bcd33ea91315db60895c2c69871d
tudor <7089284+tudddorrr@users.noreply.github.com>
2025-08-27 20:49:51 +0100

    add Talo.players.get_alias()

--- addons/talo/entities/player.gd
+++ addons/talo/entities/player.gd
@@ -2,6 +2,7 @@ class_name TaloPlayer extends TaloEntityWithProps
 ## @tutorial: https://docs.trytalo.com/docs/godot/player-props
 
 var id: String
+var aliases: Array[TaloPlayerAlias] = []
 var groups: Array[TaloPlayerGroupStub] = []
 
 var _offline_data: Dictionary
@@ -15,7 +16,10 @@ func update_from_raw_data(data: Dictionary) -> void:
 	props.assign(data.props.map(func (prop): return TaloProp.new(prop.key, prop.value)))
 
 	id = data.id
+	if data.has("aliases"):
+		aliases.assign(data.aliases.map(func (alias): return TaloPlayerAlias.new(alias)))
 	groups.assign(data.groups.map(func (group): return TaloPlayerGroupStub.new(group.id, group.name)))
+
 	_offline_data = data
 
 ## Set a property by key and value. Optionally sync the player (default true) with Talo.
@@ -38,6 +42,15 @@ func is_in_talo_group_id(group_id: String) -> bool:
 func is_in_talo_group_name(group_name: String) -> bool:
 	return not groups.filter(func (group: TaloPlayerGroupStub): return group.name == group_name).is_empty()
 
-## Get the offline data for the player
+## Get the offline data for the player.
 func get_offline_data() -> Dictionary:
 	return _offline_data
+
+## Get the first alias that matches an optional service.
+func get_alias(service: String = "") -> TaloPlayerAlias:
+	if service == "":
+		return null if aliases.is_empty() else aliases[0]
+	for alias in aliases:
+		if alias.service == service:
+			return alias
+	return null
