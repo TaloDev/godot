66377f1312f7b20d2b61a83e1859985bf11129a0
tudor <7089284+tudddorrr@users.noreply.github.com>
2025-08-28 08:29:40 +0100

    0.34.0

--- addons/talo/talo_client.gd
+++ addons/talo/talo_client.gd
@@ -1,7 +1,7 @@
 class_name TaloClient extends Node
 
 # automatically updated with a pre-commit hook
-const TALO_CLIENT_VERSION = "0.33.0"
+const TALO_CLIENT_VERSION = "0.34.0"
 
 var _base_url: String
 
