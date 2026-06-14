44ea85758582c15c9948e61eb5d4409d5da33e1b
tudor <7089284+tudddorrr@users.noreply.github.com>
2026-06-14 22:20:13 +0100

    only allow 1 in-flight auth refresh request

--- addons/talo/talo_client.gd
+++ addons/talo/talo_client.gd
@@ -30,7 +30,7 @@ func _build_response(http_request: HTTPRequest) -> TaloClientResponse:
 	return TaloClientResponse.new(res[0], res[1], res[2], res[3])
 
 func _attempt_refresh(url: String, body: Dictionary) -> Error:
-	if Talo.current_alias == null or url.ends_with("/players/auth/refresh") or not body.has("errorCode"):
+	if Talo.current_alias == null or url.ends_with("/v1/players/auth/refresh") or not body.has("errorCode"):
 		return ERR_SKIP
 
 	var error := TaloAuthError.new(body["errorCode"])
@@ -98,7 +98,7 @@ func make_request(
 	}
 
 	if ret.status >= 400:
-		if await _attempt_refresh(url, ret.body) == OK:
+		if await _attempt_refresh(full_url, ret.body) == OK:
 			return await make_request(method, url, body, headers, continuity)
 
 		handle_error(method, url, ret)
