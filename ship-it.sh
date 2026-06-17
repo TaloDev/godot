b603ba3626071f4e78c0ddfa56b8f3afb38cf78b
tudor <7089284+tudddorrr@users.noreply.github.com>
2026-06-17 06:36:39 +0100

    add push stage to ship-it

--- ship-it.sh
+++ ship-it.sh
@@ -3,6 +3,8 @@
 VERSION=$(grep '^version=' addons/talo/plugin.cfg | sed 's/version="\(.*\)"/\1/')
 REPO=$(gh repo view --json nameWithOwner -q .nameWithOwner)
 
+git push
+
 gh pr create \
   --repo "$REPO" \
   --base main \
